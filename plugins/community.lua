df054d96f667bd87af6108b60510ede32ad89ecd
einarcarias <einarcarias@gmail.com>
2023-05-02 08:47:35 +0100

    added copilot

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,6 +5,7 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   { import = "astrocommunity.colorscheme.kanagawa" },
+  { import = "astrocommunity.completion.copilot-lua" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.motion.mini-move" },
